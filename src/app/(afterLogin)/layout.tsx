5c18acee20faaab77965014e0515552ae44940bd
suniiizz <tptpmm@gmail.com>
2024-04-17 15:42:37 +0900

    레이아웃 클론

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,8 +1,44 @@
-export default async function AfterLoginLayout({ children }) {
+import { ReactNode } from "react";
+import style from "@/app/(afterLogin)/layout.module.css";
+import Link from "next/link";
+
+export default async function AfterLoginLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
-    <div>
-      애프터 로그인 레이아웃
-      {children}
+    <div className={style.container}>
+      <header className={style.leftSectionWrapper}>
+        <section className={style.leftSection}>
+          <div className={style.leftSectionFixed}>
+            <Link className={style.logo} href="/home">
+              <div className={style.logoPill}>
+                <svg width={40} viewBox="0 0 24 24" aria-hidden="true">
+                  <g>
+                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+                  </g>
+                </svg>
+              </div>
+            </Link>
+          </div>
+        </section>
+      </header>
+      <div className={style.rightSectionWrapper}>
+        <div className={style.rightSectionInner}>
+          <main className={style.main}>{children}</main>
+          <section className={style.rightSection}>
+            <form className={style.search}>
+              <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
+                <g>
+                  <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                </g>
+              </svg>
+              <input type="search" />
+            </form>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
