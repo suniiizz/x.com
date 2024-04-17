2cdf955168427a3483623d437dc039fb8cb8c4e3
suniiizz <tptpmm@gmail.com>
2024-04-17 16:21:11 +0900

    useSelectedLayoutSegment

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import style from "@/app/(afterLogin)/layout.module.css";
 import Link from "next/link";
+import NavMenu from "@/app/(afterLogin)/_component/NavMenu";
+import LogoutButton from "./_component/LogoutButton";
 
 export default async function AfterLoginLayout({
   children,
@@ -14,13 +16,22 @@ export default async function AfterLoginLayout({
           <div className={style.leftSectionFixed}>
             <Link className={style.logo} href="/home">
               <div className={style.logoPill}>
-                <svg width={40} viewBox="0 0 24 24" aria-hidden="true">
+                <svg width={30} viewBox="0 0 24 24" aria-hidden="true">
                   <g>
                     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
                   </g>
                 </svg>
               </div>
             </Link>
+            <nav>
+              <ul>
+                <NavMenu />
+              </ul>
+              <Link href="/compose/tweet" className={style.postButton}>
+                게시하기
+              </Link>
+            </nav>
+            <LogoutButton />
           </div>
         </section>
       </header>
---
+++ src/app/(afterLogin)/messages/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>쪽지 페이지</main>;
+}
